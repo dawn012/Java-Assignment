75f34b23fef60e71fefdb1cf2df724ba8b6911de
dawn012 <zhengyang040910@gmail.com>
2023-09-16 11:32:52 +0800

    first

--- Payment/Stripe.java
+++ Payment/Stripe.java
@@ -5,6 +5,6 @@ import com.stripe.param.PaymentMethodCreateParams;
 
 public class Stripe {
     public static void main(String[] args) {
-        System.out.println("hi");
+        System.out.println("hello world");
     }
 }
