456bafb00be097598b104fb0b23ffb666713c57b
LitXuan <tanklit689@gmail.com>
2023-09-18 00:21:41 +0800

    add File

---
+++ Security_Management/Connect/DatabaseUtils.java
@@ -0,0 +1,100 @@
+package Connect;
+
+import java.sql.*;
+
+public class DatabaseUtils {
+    private static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/cinema";
+    private static final String DB_UNAME = "root";
+    private static final String DB_PSD = "qwerty";
+
+    // 不能创建对象
+    private DatabaseUtils() {
+    }
+
+    public static Connection getConnection() throws SQLException {
+        // 获取连接
+        return DriverManager.getConnection(DB_URL, DB_UNAME, DB_PSD);
+    }
+
+    public static ResultSet selectQueryById(String selectedThing, String tableName, String idColumn, Object... params) throws SQLException {
+        try {
+            Connection conn = getConnection();
+
+            String sql;
+            PreparedStatement stmt;
+
+            if (params == null) {
+                // Select statement without ID
+                sql = "SELECT " + selectedThing + " FROM " + tableName;
+                stmt = conn.prepareStatement(sql);
+            }
+            else {
+                // Select statement by ID
+                sql = sql = "SELECT " + selectedThing + " FROM " + tableName + " WHERE " + idColumn;
+                stmt = conn.prepareStatement(sql);
+                for (int i = 0; i < params.length; i++) {
+                    stmt.setObject(i + 1, params[i]);  // 预处理语句中的参数索引从 1 开始，所以在循环中使用 i + 1 来为每个参数设置对应的索引
+                }
+            }
+
+            return stmt.executeQuery();
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+            throw new SQLException("Error executing SELECT query: " + e.getMessage());
+        }
+    }
+
+    public static int insertQuery(String sql, Object... params) throws SQLException {
+        try {
+            Connection conn = getConnection();
+
+            PreparedStatement stmt = conn.prepareStatement(sql);
+
+            for (int i = 0; i < params.length; i++) {
+                stmt.setObject(i + 1, params[i]);  // 预处理语句中的参数索引从 1 开始，所以在循环中使用 i + 1 来为每个参数设置对应的索引
+            }
+            return stmt.executeUpdate();
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+            throw new SQLException("Error executing INSERT query: " + e.getMessage());
+        }
+    }
+
+    public static int updateQuery(String sql, Object... params) throws SQLException {
+        try {
+            Connection conn = getConnection();
+
+            PreparedStatement stmt = conn.prepareStatement(sql);
+
+            for (int i = 0; i < params.length; i++) {
+                stmt.setObject(i + 1, params[i]);  // 预处理语句中的参数索引从 1 开始，所以在循环中使用 i + 1 来为每个参数设置对应的索引
+            }
+            return stmt.executeUpdate();
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+            throw new SQLException("Error executing UPDATE query: " + e.getMessage());
+        }
+    }
+
+    public static int deleteQueryById(String tableName, String statusColumnName, String idColumn, Object... params) throws SQLException {
+        try {
+            Connection conn = getConnection();
+
+            String sql = "UPDATE " + tableName + " SET " + statusColumnName + " = 0 WHERE " + idColumn + " = ?";
+
+            PreparedStatement stmt = conn.prepareStatement(sql);
+
+            for (int i = 0; i < params.length; i++) {
+                stmt.setObject(i + 1, params[i]);  // 预处理语句中的参数索引从 1 开始，所以在循环中使用 i + 1 来为每个参数设置对应的索引
+            }
+            return stmt.executeUpdate();
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+            throw new SQLException("Error executing DELETE query: " + e.getMessage());
+        }
+    }
+}
---
+++ Security_Management/RDriver/RDriver.java
@@ -0,0 +1,46 @@
+package RDriver;
+
+import User.Admin;
+import User.Customer;
+import User.*;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static User.Admin.getAllUsers;
+
+public class RDriver {
+
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+
+        ArrayList<User> userList = getAllUsers();
+        String usernameToFind = "admin";
+
+        User user = Customer.getUserByUsername(userList, usernameToFind);
+
+
+        if (user != null) {
+            if (user instanceof Customer) {
+                Customer customer = (Customer) user;
+                System.out.println("Found Customer: " + customer.getLogin().getUsername());
+                System.out.println(customer);
+
+                customer.modifyCustInfo(input,customer);
+                System.out.println(customer);
+
+            } else if (user instanceof Admin) {
+                Admin admin = (Admin) user;
+                System.out.println("Found Admin: " + admin.getLogin().getUsername());
+                System.out.println(admin);
+
+                admin.modifyAdminInfo(input,admin);
+                System.out.println(admin);
+            }
+        } else {
+            System.out.println("User with username " + usernameToFind + " not found.");
+        }
+
+
+    }
+}
