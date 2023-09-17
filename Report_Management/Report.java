ef010d40389e1207065a1a54a3ebbac2d27a63dd
dawn012 <zhengyang040910@gmail.com>
2023-09-17 22:12:03 +0800

    update report

--- Report_Management/Report.java
+++ Report_Management/Report.java
@@ -1,11 +1,71 @@
 package Report_Management;
 
 import Driver.DateTime;
-import Movie_Management.Movie;
 
 public abstract class Report {
     private String title;
     private DateTime reportDate;
     private String purpose;
     private String conclusion;
+
+    // Constructor
+    public Report() {
+    }
+
+    public Report(String title, DateTime reportDate, String purpose, String conclusion) {
+        this.title = title;
+        this.reportDate = reportDate;
+        this.purpose = purpose;
+        this.conclusion = conclusion;
+    }
+
+    //public abstract double calculate();
+
+    public String toString() {
+        return String.format("Title: %s\t\t\t\tReport Date: %s\n\nPurpose: \n%s\nConclusion: \n%s", title, reportDate.getDate(), purpose, conclusion);
+    }
+
+    public boolean equals(Object obj){
+        if(obj instanceof Report) {
+            Report anotherObj = (Report) obj;
+            return title.equals(anotherObj.title);
+        }
+        else {
+            return false;
+        }
+    }
+
+    // Setter
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setReportDate(DateTime reportDate) {
+        this.reportDate = reportDate;
+    }
+
+    public void setPurpose(String purpose) {
+        this.purpose = purpose;
+    }
+
+    public void setConclusion(String conclusion) {
+        this.conclusion = conclusion;
+    }
+
+    // Getter
+    public String getTitle() {
+        return title;
+    }
+
+    public DateTime getReportDate() {
+        return reportDate;
+    }
+
+    public String getPurpose() {
+        return purpose;
+    }
+
+    public String getConclusion() {
+        return conclusion;
+    }
 }
--- Report_Management/TopMovieReport.java
+++ Report_Management/TopMovieReport.java
@@ -3,35 +3,69 @@ package Report_Management;
 import Booking_Management.Booking;
 import Database.DatabaseUtils;
 import Driver.DateTime;
-import Driver.Name;
-import Genre_Management.Genre;
 import Movie_Management.Movie;
-import Movie_Management.MovieUtils;
 import Schedule_Management.Schedule;
 import Ticket_Managemnet.Ticket;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
+import java.time.LocalDate;
+import java.util.*;
 
 public class TopMovieReport extends Report {
-    private Movie movie;
-    private double totalRevenue;
+    private ArrayList<Movie> movies;
+    private ArrayList<Double> totalBoxOffices;
+    private ArrayList<Integer> numOfScreenings;
+    private ArrayList<Double> averageBoxOffices;
 
-    public static void main(String[] args) {
-        ArrayList<Movie> movies = MovieUtils.getMovieListAfterFiltered(null, null, 1);
+    // Constructor
+    public TopMovieReport() {
+        movies = new ArrayList<>();
+        totalBoxOffices = new ArrayList<>();
+        numOfScreenings = new ArrayList<>();
+        averageBoxOffices = new ArrayList<>();
+    }
 
-        for (int i = 0; i < 1; i++) {
+    public TopMovieReport(String title, DateTime reportDate, String purpose, String conclusion, ArrayList<Movie> movies, ArrayList<Double> totalBoxOffices, ArrayList<Integer> numOfScreenings, ArrayList<Double> averageBoxOffices) {
+        super(title, reportDate, purpose, conclusion);
+        this.movies = movies;
+        this.totalBoxOffices = totalBoxOffices;
+        this.numOfScreenings = numOfScreenings;
+        this.averageBoxOffices = averageBoxOffices;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder(super.toString()); // 获取父类 toString() 的结果
+
+        result.append(String.format("\n\n%-5s %-25s %-5s %-5s %s", "Ranking", "Movie Name", "Total Box Office", "Number of Screenings", "Average Box Office"));
+
+        // 遍历电影列表，将电影信息添加到结果中
+        for (int i = 0; i < movies.size(); i++) {
+            result.append(String.format("%-5d %-25s %-5.2f %-5d %.2f", (i + 1), movies.get(i).getMvName().getName(), totalBoxOffices.get(i), numOfScreenings.get(i), averageBoxOffices.get(i)));
+        }
+
+        return result.toString(); // 返回完整的字符串
+    }
+
+    public void setReportValue(LocalDate expectedDate, ArrayList<Movie> moviesList) {
+        for (int i = 0; i < moviesList.size(); i++) {
+            ArrayList<Schedule> schedules = new ArrayList<>();
+
             try {
-                Object[] params = {7};
-                ResultSet result = DatabaseUtils.selectQueryById("schedule_id", "timeTable", "movie_id = ?", params);
+                Object[] params = {moviesList.get(i).getMovieID()};
+                ResultSet result = DatabaseUtils.selectQueryById("*", "timeTable", "movie_id = ?", params);
 
                 while (result.next()) {
                     Schedule schedule = new Schedule();
 
-                    schedule.setScheduleID(result.getInt("schedule_id"));
+                    schedule.setShowDate(new DateTime(result.getDate("movie_showDate").toLocalDate()));
 
-                    //schedules.add(schedule);
+                    if (schedule.getShowDate().getDate().equals(expectedDate) || (schedule.getShowDate().getDate().isAfter(expectedDate) && schedule.getShowDate().getDate().isBefore(LocalDate.now().plusDays(1)))) {
+                        schedule.setScheduleID(result.getInt("schedule_id"));
+
+                        schedules.add(schedule);
+                    }
                 }
 
                 result.close();
@@ -39,11 +73,36 @@ public class TopMovieReport extends Report {
             catch (SQLException e) {
                 e.printStackTrace();
             }
+
+            if (!schedules.isEmpty()) {
+                movies.add(moviesList.get(i));
+                numOfScreenings.add(schedules.size());
+
+                calculateTotalRevenue(schedules);
+            }
         }
     }
 
-    public double calculateTotalRevenue() {
-        ArrayList<Schedule> schedules = new ArrayList<>();
+    /*public static ArrayList<Report> getRanking(Report report) {
+        ArrayList<Report> reportAfterRanking = new ArrayList<>(reports);
+
+        for (int i = 0; i < reports.size(); i++) {
+            for (int j = i + 1; j < reports.size(); j++) {
+                TopMovieReport report1 = (TopMovieReport) reports.get(i);
+                TopMovieReport report2 = (TopMovieReport) reports.get(j);
+
+                if (report1.getTotalBoxOffices() < report2.getTotalBoxOffices()) {
+                    // 交换位置
+                    reportAfterRanking.set(i, report2);
+                    reportAfterRanking.set(j, report1);
+                }
+            }
+        }
+
+        return reportAfterRanking;
+    }*/
+
+    private void calculateTotalRevenue(ArrayList<Schedule> schedules) {
         ArrayList<Ticket> tickets = new ArrayList<>();
         ArrayList<Booking> bookings = new ArrayList<>();
 
@@ -89,11 +148,88 @@ public class TopMovieReport extends Report {
             }
         }
 
-        double totalPrice = 0;
+        double totalBoxOffice = 0;
         for (int i = 0; i < bookings.size(); i++) {
-            totalPrice += bookings.get(i).getTotalPrice();
+            totalBoxOffice += bookings.get(i).getTotalPrice();
         }
 
-        return totalPrice;
+        totalBoxOffices.add(totalBoxOffice);
+
+        double averageBoxOffice = totalBoxOffice / schedules.size();
+        averageBoxOffices.add(averageBoxOffice);
+    }
+
+    public static Report getRanking(Report report) {
+        // 创建一个包含 Movie 和总票房的 Map
+        Map<Movie, Double> movieBoxOffices = new HashMap<>();
+
+        for (int i = 0; i < ((TopMovieReport)report).movies.size(); i++) {
+            Movie movie = ((TopMovieReport)report).movies.get(i);
+            double totalBoxOffice = ((TopMovieReport)report).totalBoxOffices.get(i);
+            movieBoxOffices.put(movie, totalBoxOffice);
+        }
+
+        // 将 Map 转换为 List 进行排序
+        List<Map.Entry<Movie, Double>> sortedList = new ArrayList<>(movieBoxOffices.entrySet());
+
+        // 根据总票房降序排列
+        Collections.sort(sortedList, new Comparator<Map.Entry<Movie, Double>>() {
+            @Override
+            public int compare(Map.Entry<Movie, Double> entry1, Map.Entry<Movie, Double> entry2) {
+                return Double.compare(entry2.getValue(), entry1.getValue());
+            }
+        });
+
+        // 创建一个新的报告对象
+        Report reportAfterRanking = new TopMovieReport();
+
+        // 找到票房最高的电影
+        for (int i = 0; i < sortedList.size(); i++) {
+            Movie rank = sortedList.get(i).getKey();
+
+            // 将最高票房电影添加到新报告中
+            ((TopMovieReport)reportAfterRanking).getMovie().add(rank);
+            ((TopMovieReport)reportAfterRanking).getTotalBoxOffices().add(movieBoxOffices.get(rank));
+            ((TopMovieReport)reportAfterRanking).getNumOfScreenings().add(((TopMovieReport)report).numOfScreenings.get(((TopMovieReport)report).movies.indexOf(rank)));
+            ((TopMovieReport)reportAfterRanking).getAverageBoxOffices().add(((TopMovieReport)report).averageBoxOffices.get(((TopMovieReport)report).movies.indexOf(rank)));
+        }
+
+        return reportAfterRanking;
+    }
+
+    public void printing() {
+        toString();
+    }
+
+    public void setMovie(ArrayList<Movie> movies) {
+        this.movies = movies;
+    }
+
+    public void setTotalBoxOffices(ArrayList<Double> totalBoxOffices) {
+        this.totalBoxOffices = totalBoxOffices;
+    }
+
+    public void setNumOfScreenings(ArrayList<Integer> numOfScreenings) {
+        this.numOfScreenings = numOfScreenings;
+    }
+
+    public void setAverageBoxOffices(ArrayList<Double> averageBoxOffices) {
+        this.averageBoxOffices = averageBoxOffices;
+    }
+
+    public ArrayList<Movie> getMovie() {
+        return movies;
+    }
+
+    public ArrayList<Double> getTotalBoxOffices() {
+        return totalBoxOffices;
+    }
+
+    public ArrayList<Integer> getNumOfScreenings() {
+        return numOfScreenings;
+    }
+
+    public ArrayList<Double> getAverageBoxOffices() {
+        return averageBoxOffices;
     }
 }
