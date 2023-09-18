2763cb78fb89da61116e1a0c8ecc8b5ad237aa04
LitXuan <tanklit689@gmail.com>
2023-09-18 11:17:23 +0800

    add file

---
+++ Security_Management/AdminDriver.java
@@ -0,0 +1,91 @@
+package RDriver;
+
+import User.*;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AdminDriver {
+
+    public static void main(String[] args) {
+        ArrayList<User> userList = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        User admin = new Admin();
+        int choice;
+
+
+        do {
+            printMenu();
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("You selected Option 1.");
+                    Admin.createAdmin(scanner);
+                    break;
+                case 2:
+                    System.out.println("You selected Option 2.");
+                    boolean submenuActive = true;
+                    while (submenuActive) {
+                        System.out.println("Submenu Options:");
+                        System.out.println("1. View All Users");
+                        System.out.println("2. View All Customer");
+                        System.out.println("3. View All Admin");
+                        System.out.println("0. Back to Main Menu");
+                        System.out.print("Choose an option: ");
+                        int submenuChoice = scanner.nextInt();
+                        switch (submenuChoice) {
+                            case 1:
+                                System.out.println("You selected Submenu Option 1.");
+                                ((Admin) admin).viewAllUsers();
+                                break;
+                            case 2:
+                                System.out.println("You selected Submenu Option 2.");
+                                ((Admin) admin).viewAllCustomers();
+                                break;
+                            case 3:
+                                System.out.println("You selected Submenu Option 3.");
+                                ((Admin) admin).viewAllAdmins();
+                                break;
+                            case 0:
+                                System.out.println("Returning to Main Menu.");
+                                submenuActive = false; // 退出子菜单
+                                break;
+                            default:
+                                System.out.println("Invalid option. Please choose again.");
+                        }
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("You selected Option 3.");
+                    ((Admin) admin).manageAccountStatus();
+                    break;
+                case 4:
+                    System.out.println("You selected Option 4.");
+                    ((Admin) admin).viewAllUsers();
+                    ((Admin) admin).deleteUserById(scanner);
+                    break;
+                case 5:
+                    System.out.println("Exiting the program.");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please select a valid option.");
+            }
+
+        } while (choice != 5);
+
+        scanner.close();
+    }
+
+    public static void printMenu() {
+        System.out.println("Menu:");
+        System.out.println("1. Administrator Registration");
+        System.out.println("2. View Users Information");
+        System.out.println("3. Manage Account Status");
+        System.out.println("4. Remove Users");
+        System.out.println("5. Exit");
+    }
+}
---
+++ Security_Management/User.java
@@ -0,0 +1,106 @@
+package User;
+
+import Login.Login;
+import Validator.RegisterValidator;
+
+import java.util.Scanner;
+public abstract class User{
+
+    private Login login;
+    private String email;
+    private String DOB;
+    private String userType;
+
+
+    public abstract void add();
+
+    public User(Login login, String email, String DOB, String userType) {
+        this.login = login != null ? login : new Login();
+        this.email = email;
+        this.DOB = DOB;
+        this.userType = userType;
+    }
+
+
+    public User() {
+    }
+
+    public static User registerUser(Scanner input) {
+        User newUser = new Customer();
+
+        System.out.println("Please Enter Username: ");
+        String username = RegisterValidator.validateUsername(input);
+        newUser.getLogin().setUsername(username);
+
+        System.out.println("Please Enter Password: ");
+        String password = RegisterValidator.validatePassword(input);
+        newUser.getLogin().setPassword(password);
+
+        System.out.println("Please Confirm Your Password: ");
+        String confirmPassword = RegisterValidator.validatePasswordConfirmation(input, password);
+
+        System.out.println("Please Enter Your email: ");
+        String email = RegisterValidator.validateEmail(input);
+        ((Customer)newUser).setEmail(email);
+
+        input.nextLine();
+
+        System.out.println("Please Enter Date of Birth(01-01-1990): ");
+        String dob = RegisterValidator.validateDateOfBirth(input);
+        newUser.setDOB(dob);
+
+        newUser.add();
+
+        return newUser;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "login=" + login +
+                ", email='" + email + '\'' +
+                ", DOB='" + DOB + '\'' +
+                ", userType='" + userType + '\'' +
+                '}';
+    }
+    public int getUserId() {
+        return 0;
+    }
+    public Login getLogin() {
+        if (login == null) {
+            login = new Login();
+        }
+        return login;
+    }
+
+
+    public void setLogin(Login login) {
+        this.login = login;
+    }
+
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setUserType(String usertype) {
+        this.userType = usertype;
+    }
+
+}
